ad24870ad649edbf10bef024a31b5c46561371e3
RajpootNeha <rajpoot0211neha@gmail.com>
2018-10-04 09:54:41 +0530

    Palindrome

---
+++ palindrome.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+read num
+rev=""
+temp=$num
+while [ $num -gt 0 ]
+do
+	s=$(( $num % 10 ))
+	num=$(( $num/10 ))
+	rev=$(echo ${rev}${s})
+done
+if [ $temp -eq $rev ];
+then
+	echo "Number is Palindrome"
+else
+	echo "Number is not Palindrome"
+fi
